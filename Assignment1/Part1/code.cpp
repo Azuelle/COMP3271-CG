b1d7a4fd2b5a2800d3e9f38c6eb63c1c96256d95
夢野 雫 <tjk@live.cn>
2024-10-09 19:03:20 +0800

    Implement assignment 1.1

--- Assignment1/Part1/code.cpp
+++ Assignment1/Part1/code.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <vector>
 
-
 //////////////////////////////////////////////////////
 // The Global Variables To Be Used
 
@@ -29,20 +28,11 @@ int point_count = 0;
 
 // color array for triangles
 // size is 11. So color_index should range from 0 to 10 for triangles.
-double color_array[][3] = {
-    {0.9,0,0}, //red
-    {0,0.5,0.4},
-    {0.1,0.2,0.46},
-    {0.9,0.9,0},
-    {0,1.0,0},
-    {0,1.0,1.0},
-    {0,0,1.0},
-    {1.0,0,1.0},
-    {0.9,0.6,0},
-    {0.9,1.0,0.6},
-    {0.2,0.2,0.2}
-};
-
+double color_array[][3] = {{0.9, 0, 0},  // red
+                           {0, 0.5, 0.4},  {0.1, 0.2, 0.46}, {0.9, 0.9, 0},
+                           {0, 1.0, 0},    {0, 1.0, 1.0},    {0, 0, 1.0},
+                           {1.0, 0, 1.0},  {0.9, 0.6, 0},    {0.9, 1.0, 0.6},
+                           {0.2, 0.2, 0.2}};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // This function is called to clear triangles.
@@ -59,29 +49,46 @@ void DrawTriangles() {
     // uncomment this line if you would like to unfill triangles
     // glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 
-    // sample code to draw vertices of triangle
-    glColor3d(1.0, 1.0, 1.0);
     glPointSize(4);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < point_count; i++) {
-        glVertex2d(triangle_to_draw.vertices[i][0], triangle_to_draw.vertices[i][1]);
+
+    // Draw previous triangles
+    glBegin(GL_TRIANGLES);
+    for (Triangle t : triangles) {
+        glColor3d(color_array[t.color_index][0], color_array[t.color_index][1],
+                  color_array[t.color_index][2]);
+        for (int i = 0; i < 3; i++) {
+            glVertex2d(t.vertices[i][0], t.vertices[i][1]);
+        }
     }
     glEnd();
 
-    // TODO: Add code to draw triangles here. Use triangles.size() to get number of triangles.
-    // Use triangles[i] to get the ith triangle.
-    // Remember to set current gl color from the color_array.
-    // If the number of triangles exceeds the length of the color list, you can start iterating the color list from the beginning again.
+    // Draw vertices of current triangle
+    glBegin(GL_POINTS);
+    int next_triangle_idx = triangles.size();
+    glColor3d(color_array[next_triangle_idx % 11][0],
+              color_array[next_triangle_idx % 11][1],
+              color_array[next_triangle_idx % 11][2]);
+    for (int i = 0; i < point_count; i++) {
+        glVertex2d(triangle_to_draw.vertices[i][0],
+                   triangle_to_draw.vertices[i][1]);
+    }
+    glEnd();
 }
-
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // This function is called to handle mouse left click events.
-// m_x and m_y are the x and y coordinates of the clicked point in OpenGL coordinate system.
+// m_x and m_y are the x and y coordinates of the clicked point in OpenGL
+// coordinate system.
 
 void MouseInteraction(double m_x, double m_y) {
-    // TODO: Store the point temporarily into the variable triangle_to_draw.
-    // When 3 points are specified, we get a new triangle.
-    // Store the points of the triangle into a new element of the list 'triangles'
-    // Use push_back function from std::Vector to add new triangles to the list
+    // Add new point to the temporary triangle
+    triangle_to_draw.vertices[point_count][0] = m_x;
+    triangle_to_draw.vertices[point_count][1] = m_y;
+    point_count++;
+
+    // When 3 points are specified, we get a new triangle
+    if (point_count != 3) return;
+    triangle_to_draw.color_index = triangles.size() % 11;
+    triangles.push_back(Triangle(triangle_to_draw));
+    point_count = 0;
 }
